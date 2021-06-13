5dd721fd29347e070e8c628864407b76310e2149
Edward Luo <edluo1@bu.edu>
2021-06-13 09:55:55 -0700

    Made buttons work in main menu

---
+++ Assets/Scripts/Mechanics/Start.cs
@@ -0,0 +1,11 @@
+
+using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+public class Start: MonoBehaviour {  
+    public void LoadMainGame() {  
+        SceneManager.LoadScene("MainLevel");  
+    }  
+
+}   
